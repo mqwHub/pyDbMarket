568886366b2523b42b53f137fb2d4d9487ad415f
mqwHub <lcocsu@gmail.com>
2015-11-11 21:16:17 +0800

    x commit

--- src/db.py
+++ src/db.py
@@ -4,6 +4,8 @@ import urllib
 import urllib2
 import cookielib
 import re
+import json
+import requests
 
 
 class DB(object):
@@ -19,6 +21,8 @@ class DB(object):
         self.response = self.opener.open(self.url, urllib.urlencode(self.post))
 
     def login(self):
+        print self.response.geturl()
+        print self.url
         if self.response.geturl() == self.url:
             print 'logining...'
             html = self.response.read()
@@ -39,9 +43,50 @@ class DB(object):
     def getMainPage(self):
         url = "http://www.douban.com"
         resp  = self.opener.open(url)
-        print resp.read()      
+        print resp.read()  
+        
+    def getSidsByPageNum(self, pageNum):
+        _market_api_url = "https://market.douban.com/api/home/shops?category=index&page=" + str(pageNum) + "&page_size=12"
+        r = self.opener.open(_market_api_url)
+        jsontext = json.load(r)
+        return jsontext['data']['shops'][0]['sid']
+    
+    def followShop(self, shopName):
+        _follow_shop_url = "https://market.douban.com/api/shops/" + shopName + "/follow"
+        _follow_post_data = {'ck':"xV-v"}
+        r = self.opener.open(_follow_shop_url, urllib.urlencode(_follow_post_data))
+        print json.load(r)
+    
+    def getAllShops(self):
+        shoplist = []
+        for page in range(1, 12):
+            _market_api_url = "https://market.douban.com/api/home/shops?category=index&page=" + str(page) + "&page_size=12"
+            r = self.opener.open(_market_api_url)
+            jsontext = json.load(r)
+            shops = jsontext['data']['shops']
+            total = jsontext['data']['total']
+            for i in range(total):
+                shoplist.append(shops[i]['sid'])
+        return shoplist   
+            
+        
+    def followAllShop(self):
+        shops = self.getAllShops()
+        for shop in shops:
+            _follow_shop_url = "https://market.douban.com/api/shops/" + shop + "/follow"
+            _follow_post_data = {'ck':"xV-v"}
+            r = self.opener.open(_follow_shop_url, urllib.urlencode(_follow_post_data))
+            result = json.load(r)
+            if result['r'] == 0:
+                print 'follow the shop ' + shop + ' success.'
+     
+def test():
+    url = "https://market.douban.com/api/home/shops?category=index&page=2&page_size=12"
+    r = requests.get(url)
+    print r.json()
+    
 email = raw_input('Your email: ')
 passwd = raw_input('Your passwd: ')   
 my = DB(email, passwd)         
 my.login()
-my.getMainPage()
+my.followAllShop()
---
+++ src/test.py
@@ -0,0 +1,5 @@
+text = {'a':'1',
+        'b':'2',
+        'c':'3'}
+
+print len(text.keys())
